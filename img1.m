b929964a9ef5df20e0effd2518399075897ade45
Ritika Giridhar <57015521+trashsock@users.noreply.github.com>
2022-05-03 18:55:35 +0530

    error fixes

--- img1.m
+++ img1.m
@@ -1,34 +1,32 @@
 %for image 1
-clc;    % Clear the command window.
-close all;  % Close all figures (except those of imtool.)
-workspace;  % Make sure the workspace panel is showing.
+clc;   
+close all;  
+workspace;  
 format long g;
 format compact;
 fontSize = 10;
 fullFileName = "images.jfif";
 [folder, baseFileName, ext] = fileparts(fullFileName);
-% Read in demo image.
+% read image
 rgbImage = imread(fullFileName);
 [rows, columns, numberOfColorChannels] = size(rgbImage);
-% Display the original image.
+% display original image
 subplot(2, 2, 1);
 imshow(rgbImage, []);
 axis on;
 caption = sprintf('Original Color Image, %s', baseFileName);
 title(caption, 'FontSize', fontSize, 'Interpreter', 'None');
 drawnow;
-% Set up status line to see values when you mouse over the image.
 hp = impixelinfo(); 
-% Enlarge figure to full screen.
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0.05 1 0.95]);
-% Display the image.
+% display image
 subplot(2, 2, 4);
 imshow(mask, []);
 axis on;
 title('Mask Image', 'FontSize', fontSize, 'Interpreter', 'None');
 hp = impixelinfo();
 drawnow;
-subplot(2, 2, 1); % Switch back to the original image in the upper left.
+subplot(2, 2, 1);
 hold on;
 boundaries = bwboundaries(mask);
 numberOfBoundaries = size(boundaries, 1);
